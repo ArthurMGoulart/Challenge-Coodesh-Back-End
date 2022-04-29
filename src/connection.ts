46885d21231e2f94de2303f422909a07d333374e
ArthurMGoulart <budiarthur@gmail.com>
2022-04-29 16:48:02 -0300

    feat: docker-compose, .env

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,8 @@
+require('dot-env');
+
 import mongoose from 'mongoose';
 
-const uri = 'mongodb+srv://Budi:coodesh@cluster0.iafrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.iafrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectToDatabase = () => mongoose.connect(uri);
 
