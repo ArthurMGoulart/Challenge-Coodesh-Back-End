fadc5e3796cdce69b03e54ae2650467afb8fcde9
ArthurMGoulart <budiarthur@gmail.com>
2022-04-29 17:02:26 -0300

    remove gitigore files

--- src/connection.ts
+++ src/connection.ts
@@ -1,9 +1,11 @@
-require('dot-env');
+import dotenv from 'dotenv';
+dotenv.config();
 
 import mongoose from 'mongoose';
-
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.iafrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+ 
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iafrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectToDatabase = () => mongoose.connect(uri);
 
 export default connectToDatabase;
+ 
